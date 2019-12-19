b04d5f11ddec287392e5a242e44369afa13777c5
Оля <vesnushka.ol@mail.ru>
2019-12-19 10:53:55 +0300

    убарала дробные пиксели в отзывах, поправила ПП

--- source/js/open-modal.js
+++ source/js/open-modal.js
@@ -1,12 +1,12 @@
 var modal = document.querySelector('.modal');
-var btnModal = document.querySelector('.modal-close');
+var btnModal = document.querySelector('.modal-open');
 
 btnModal.addEventListener('click', function() {
   if (modal.classList.contains('modal--closed')) {
     modal.classList.remove('modal--closed');
     modal.classList.add('modal--opened');
   } else {
-    modal.classList.add('main-nav--closed');
+    modal.classList.add('modal--closed');
     modal.classList.remove('modal--opened');
   }
 });
