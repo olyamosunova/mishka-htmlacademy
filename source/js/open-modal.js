a18d3ff5fb37fae6b43b8c998678f98f593a04f5
Оля <vesnushka.ol@mail.ru>
2020-01-10 05:32:43 +0300

    Стили состояний для всех устройств, центрирование футера

--- source/js/open-modal.js
+++ source/js/open-modal.js
@@ -1,12 +1,47 @@
 var modal = document.querySelector('.modal');
 var btnModal = document.querySelector('.modal-open');
 
-btnModal.addEventListener('click', function() {
+btnModal.addEventListener("click", function() {
   if (modal.classList.contains('modal--closed')) {
     modal.classList.remove('modal--closed');
     modal.classList.add('modal--opened');
-  } else {
-    modal.classList.add('modal--closed');
-    modal.classList.remove('modal--opened');
   }
 });
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal.classList.contains("modal--opened")) {
+      evt.preventDefault();
+      modal.classList.remove("modal--opened");
+      modal.classList.add('modal--closed');
+    }
+  }
+});
+
+/*
+btnModal = Array.prototype.slice.call(btnModal);
+
+var closeModal = function() {
+  if (modal.classList.contains('modal--opened')) {
+    modal.classList.remove('modal--opened');
+    modal.classList.add('modal--closed');
+  }
+}
+
+btnModal.forEach(function(elem) {
+  elem.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.remove('modal--closed');
+    modal.classList.add('modal--opened');
+  });
+});
+
+modal.addEventListener("click", closeModal);
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    closeModal;
+  }
+});
+*/
